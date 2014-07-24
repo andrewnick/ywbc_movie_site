1e88740df71dc2423fc07ada4128483fa8a37c3e
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2014-07-24 14:08:38 +1200

    further formatting

--- site/includes/home_main.php
+++ site/includes/home_main.php
@@ -1,11 +1,11 @@
 <main>
 
 	<div class="home_main__bg--top background--blue">
-		<div class="container background--beige">
+		<div class="container">
 			<div class="row">
 				<div class="embed-responsive embed-responsive-16by9">
-					<video class="embed-responsive-item" controls >
-						<source src="assets/media/AWAKENING_NEW_ZEALAND.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'></source>
+					<video class="embed-responsive-item" controls>
+						<source src="assets/media/LIFE_lg.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'></source>
 					</video>
 				</div>
 			</div>
